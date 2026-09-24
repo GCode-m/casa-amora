f9ff49d74fcf129d296807642b457a5973fd1862
GCode-m <gabrielmota366@gmail.com>
2026-09-24 13:02:01 -0300

    Update image import to use HTML img tag

--- src/components/site/Hero.tsx
+++ src/components/site/Hero.tsx
@@ -1,5 +1,5 @@
 import { MapPin, Star } from "lucide-react";
-import mesaCafeManha from "@/assets/mesa-cafe-manha.jpg.asset.json";
+import mesaCafeManha from "<img src="/assets/mesa-cafe-manha.jpg" />;
 import { MAPS_DIRECTIONS } from "@/lib/constants";
 
 export function Stars({ className = "" }: { className?: string }) {
